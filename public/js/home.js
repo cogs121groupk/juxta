92ff8e8abb5b24f94d09d11bb8acd64c41519273
brian <beerye432@gmail.com>
2016-06-04 22:45:39 -0700

    added radar chart initial

--- public/js/home.js
+++ public/js/home.js
@@ -144,27 +144,155 @@ function removeFromCompare(name){
 
 function scroll(){
 
-	var ctx = $("comp1");
+	if(!compare[0] === undefined){
 
-	var data = {
-		labels: ["Overall Rating", "Approval of CEO", "Number of Ratings"],
-		datasets: [
-			{
-				label: compare[0].name,
-				data: [compare[0].overallRating, compare[0].ceo.pctApprove, compare[0].numberOfRatings]
-			},
-			{
+		var ctx = $("comp1");
 
-				label: "Average",
-				data: [3.0, 25, 200]
-			}
-		]
-	};
+		var data = {
+			labels: ["Overall Rating", "Approval of CEO", "Number of Ratings"],
+			datasets: [
+				{
+					label: compare[0].name,
+					data: [compare[0].overallRating, compare[0].ceo.pctApprove, compare[0].numberOfRatings]
+				},
+				{
 
-	var myRadarChart = new Chart(ctx, {
-		type: 'radar', 
-		data: data
-	});
+					label: "Average",
+					data: [3.0, 25, 200]
+				}
+			]
+		};
+
+		var myRadarChart = new Chart(ctx, {
+			type: 'radar', 
+			data: data
+		});
+	}
+
+	if(!compare[1] === undefined){
+
+		var ctx = $("comp2");
+
+		var data = {
+			labels: ["Overall Rating", "Approval of CEO", "Number of Ratings"],
+			datasets: [
+				{
+					label: compare[1].name,
+					data: [compare[1].overallRating, compare[1].ceo.pctApprove, compare[1].numberOfRatings]
+				},
+				{
+
+					label: "Average",
+					data: [3.0, 25, 200]
+				}
+			]
+		};
+
+		var myRadarChart = new Chart(ctx, {
+			type: 'radar', 
+			data: data
+		});
+	}
+
+	if(!compare[2] === undefined){
+
+		var ctx = $("comp3");
+
+		var data = {
+			labels: ["Overall Rating", "Approval of CEO", "Number of Ratings"],
+			datasets: [
+				{
+					label: compare[2].name,
+					data: [compare[2].overallRating, compare[2].ceo.pctApprove, compare[2].numberOfRatings]
+				},
+				{
+
+					label: "Average",
+					data: [3.0, 25, 200]
+				}
+			]
+		};
+
+		var myRadarChart = new Chart(ctx, {
+			type: 'radar', 
+			data: data
+		});
+	}
+
+	if(!compare[3] === undefined){
+
+		var ctx = $("comp4");
+
+		var data = {
+			labels: ["Overall Rating", "Approval of CEO", "Number of Ratings"],
+			datasets: [
+				{
+					label: compare[3].name,
+					data: [compare[3].overallRating, compare[3].ceo.pctApprove, compare[3].numberOfRatings]
+				},
+				{
+
+					label: "Average",
+					data: [3.0, 25, 200]
+				}
+			]
+		};
+
+		var myRadarChart = new Chart(ctx, {
+			type: 'radar', 
+			data: data
+		});
+	}
+
+	if(!compare[4] === undefined){
+
+		var ctx = $("comp5");
+
+		var data = {
+			labels: ["Overall Rating", "Approval of CEO", "Number of Ratings"],
+			datasets: [
+				{
+					label: compare[4].name,
+					data: [compare[4].overallRating, compare[4].ceo.pctApprove, compare[4].numberOfRatings]
+				},
+				{
+
+					label: "Average",
+					data: [3.0, 25, 200]
+				}
+			]
+		};
+
+		var myRadarChart = new Chart(ctx, {
+			type: 'radar', 
+			data: data
+		});
+	}
+
+	if(!compare[5] === undefined){
+
+		var ctx = $("comp6");
+
+		var data = {
+			labels: ["Overall Rating", "Approval of CEO", "Number of Ratings"],
+			datasets: [
+				{
+					label: compare[5].name,
+					data: [compare[5].overallRating, compare[5].ceo.pctApprove, compare[5].numberOfRatings]
+				},
+				{
+
+					label: "Average",
+					data: [3.0, 25, 200]
+				}
+			]
+		};
+
+		var myRadarChart = new Chart(ctx, {
+			type: 'radar', 
+			data: data
+		});
+	}
 
 	$('html, body').animate({ 
 	   scrollTop: $(document).height()-$(window).height()}, 
